a4a3f086aff48efa4238b4f96b956199780d7cda
Patrick <revmydev@gmail.com>
2019-03-22 16:19:16 -0700

    basic add ingredient functionality for shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,7 +23,8 @@ import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-i
     ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
