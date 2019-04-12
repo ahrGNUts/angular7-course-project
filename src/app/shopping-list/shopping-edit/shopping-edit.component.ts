d974eac2e5fb72dd4ac2c17db466e46d22a02e14
Patrick <revmydev@gmail.com>
2019-04-12 15:35:23 -0700

    resetting the form after submission

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -37,7 +37,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.shoppingListService.startedEditing.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -45,7 +45,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(newIngredient);
     }
-
+    form.reset();
+    this.editMode = false;
   }
 
 }
